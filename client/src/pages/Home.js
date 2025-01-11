d29c07972d3e70864380c686969d932caf753ab9
Mukul Jadon <147290552+MukulJadon20@users.noreply.github.com>
2025-01-11 19:10:06 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,75 +13,84 @@ const Home = () => {
   const navigate = useNavigate()
   const location = useLocation()
 
-  console.log('user',user)
-  const fetchUserDetails = async()=>{
-    try {
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
-        const response = await axios({
-          url : URL,
-          withCredentials : true
-        })
+  console.log('User state:', user)
 
+  const fetchUserDetails = async () => {
+    try {
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
+      const response = await axios.get(URL, { withCredentials: true })
+
+      if (response.data?.data) {
         dispatch(setUser(response.data.data))
 
-        if(response.data.data.logout){
-            dispatch(logout())
-            navigate("/email")
+        if (response.data.data.logout) {
+          dispatch(logout())
+          navigate("/email")
         }
-        console.log("current user Details",response)
+        console.log("User details fetched:", response.data.data)
+      } else {
+        console.error("User details not found in the response")
+      }
     } catch (error) {
-        console.log("error",error)
+      console.error("Error fetching user details:", error)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUserDetails()
-  },[])
+  }, [])
 
   /***socket connection */
-  useEffect(()=>{
-    const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
-      auth : {
-        token : localStorage.getItem('token')
+  useEffect(() => {
+    const socketConnection = io(process.env.REACT_APP_BACKEND_URL, {
+      auth: {
+        token: localStorage.getItem('token'),
       },
+      transports: ['websocket'], // Explicitly set transport type to WebSocket
     })
 
-    socketConnection.on('onlineUser',(data)=>{
-      console.log(data)
+    socketConnection.on('connect', () => {
+      console.log("Socket connected:", socketConnection.id)
+    })
+
+    socketConnection.on('onlineUser', (data) => {
+      console.log("Online users:", data)
       dispatch(setOnlineUser(data))
+    })
+
+    socketConnection.on('connect_error', (err) => {
+      console.error("Socket connection error:", err)
     })
 
     dispatch(setSocketConnection(socketConnection))
 
-    return ()=>{
+    return () => {
       socketConnection.disconnect()
     }
-  },[])
-
+  }, [])
 
   const basePath = location.pathname === '/'
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
-           <Sidebar/>
-        </section>
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+        <Sidebar />
+      </section>
 
-        {/**message component**/}
-        <section className={`${basePath && "hidden"}`} >
-            <Outlet/>
-        </section>
+      {/* Message Component */}
+      <section className={`${basePath && "hidden"}`} >
+        <Outlet />
+      </section>
 
-
-        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
-            <div>
-              <img
-                src={logo}
-                width={250}
-                alt='logo'
-              />
-            </div>
-            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+        <div>
+          <img
+            src={logo}
+            width={250}
+            alt='logo'
+          />
         </div>
+        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      </div>
     </div>
   )
 }
