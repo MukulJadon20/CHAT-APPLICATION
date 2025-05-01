df67c44f163e492629e77588ee8dcb19bfacc299
Mukul Jadon <147290552+MukulJadon20@users.noreply.github.com>
2025-05-01 10:51:00 +0530

    commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,115 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice'
-import Sidebar from '../components/Sidebar'
-import logo from '../assets/logo.png'
-import io from 'socket.io-client'
-
-const Home = () => {
-  const user = useSelector(state => state.user)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  console.log('user',user)
-  const fetchUserDetails = async()=>{
-    try {
-<<<<<<< HEAD
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
-        const response = await axios({
-          url : URL,
-          withCredentials : true
-        })
-=======
-      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
-      const response = await axios.get(URL, { withCredentials: true });
->>>>>>> 341b8fd060fbd6834ed31927651867f5a807febc
-
-        dispatch(setUser(response.data.data))
-
-        if(response.data.data.logout){
-            dispatch(logout())
-            navigate("/email")
-        }
-        console.log("current user Details",response)
-    } catch (error) {
-        console.log("error",error)
-    }
-  }
-
-  useEffect(()=>{
-    fetchUserDetails()
-  },[])
-
-<<<<<<< HEAD
-  /***socket connection */
-  useEffect(()=>{
-    const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
-      auth : {
-        token : localStorage.getItem('token')
-      },
-    })
-
-    socketConnection.on('onlineUser',(data)=>{
-      console.log(data)
-      dispatch(setOnlineUser(data))
-    })
-
-    dispatch(setSocketConnection(socketConnection))
-
-    return ()=>{
-      socketConnection.disconnect()
-=======
-  /*** WebSocket connection ***/
-  useEffect(() => {
-    const socketURL ="https://chat-application-8qij.onrender.com"; // Ensure this is correctly set in your environment variables
-    const token = localStorage.getItem('token');
-    if (!token) {
-      console.error('Missing authentication token');
-      return;
->>>>>>> 341b8fd060fbd6834ed31927651867f5a807febc
-    }
-  },[])
-
-  const basePath = location.pathname === '/'
-  return (
-    <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
-           <Sidebar/>
-        </section>
-
-        {/**message component**/}
-        <section className={`${basePath && "hidden"}`} >
-            <Outlet/>
-        </section>
-
-        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
-            <div>
-              <img
-                src={logo}
-                width={250}
-                alt='logo'
-              />
-            </div>
-            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
-        </div>
-    </div>
-  )
-}
-
-<<<<<<< HEAD
-export default Home
-=======
-export default Home;
-
-
-
-
-
-// /* eslint-disable react-hooks/exhaustive-deps */
 // import axios from 'axios'
 // import React, { useEffect } from 'react'
 // import { useDispatch, useSelector } from 'react-redux'
@@ -199,4 +87,122 @@ export default Home;
 // }
 
 // export default Home
->>>>>>> 341b8fd060fbd6834ed31927651867f5a807febc
+
+
+
+
+
+
+
+
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice'
+import Sidebar from '../components/Sidebar'
+import logo from '../assets/logo.png'
+import io from 'socket.io-client'
+
+const Home = () => {
+  const user = useSelector(state => state.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  console.log('User state:', user)
+
+  const fetchUserDetails = async () => {
+    try {
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
+      const response = await axios.get(URL, { withCredentials: true })
+
+      if (response.data?.data) {
+        dispatch(setUser(response.data.data))
+
+        if (response.data.data.logout) {
+          dispatch(logout())
+          navigate("/email")
+        }
+        console.log("User details fetched:", response.data.data)
+      } else {
+        console.error("User details not found in the response")
+      }
+    } catch (error) {
+      console.error("Error fetching user details:", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchUserDetails()
+  }, [])
+
+  /***socket connection */
+  useEffect(() => {
+    const socketURL = process.env.REACT_APP_BACKEND_URL
+
+    // Ensure the WebSocket URL is valid
+    if (!socketURL) {
+      console.error("WebSocket URL is not defined in the environment variables")
+      return
+    }
+
+    const socketConnection = io(socketURL, {
+      auth: {
+        token: localStorage.getItem('token'),
+      },
+      transports: ['websocket'], // Explicitly use WebSocket transport
+    })
+
+    // Log socket events for debugging
+    socketConnection.on('connect', () => {
+      console.log("WebSocket connected with ID:", socketConnection.id)
+    })
+
+    socketConnection.on('connect_error', (error) => {
+      console.error("WebSocket connection error:", error)
+    })
+
+    socketConnection.on('disconnect', (reason) => {
+      console.warn("WebSocket disconnected:", reason)
+    })
+
+    socketConnection.on('onlineUser', (data) => {
+      console.log("Online users:", data)
+      dispatch(setOnlineUser(data))
+    })
+
+    dispatch(setSocketConnection(socketConnection))
+
+    return () => {
+      socketConnection.disconnect()
+    }
+  }, [])
+
+  const basePath = location.pathname === '/'
+  return (
+    <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+        <Sidebar />
+      </section>
+
+      {/* Message Component */}
+      <section className={`${basePath && "hidden"}`} >
+        <Outlet />
+      </section>
+
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+        <div>
+          <img
+            src={logo}
+            width={250}
+            alt='logo'
+          />
+        </div>
+        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
